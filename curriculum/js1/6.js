35f15309e5b8c9b2533734291cac4aad0cae0594
Jasper <Jasper@Jaspers-Mac.local>
2018-01-27 16:29:50 -0800

    solves 6

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -5,9 +5,23 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (num, counter = 2, sum = 0) => {
+  //if counter reaches half of the number, return sum (this eliminates duplicate divisors to the sum)
+  if (counter > num/2) {
+    return console.log(sum);
+  }
+
+  //if check to add to sum if counter number is a divisor
+  if (num % counter === 0) {
+    sum += counter;
+  } 
+
+  //increment counter and run again
+  counter += 1;
+  return solution(num, counter, sum);
 };
+
+solution(10);
 
 module.exports = {
   solution,
