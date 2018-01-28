3c10ee66e33256de4287f53a41cef6b2533ae085
Jasper <Jasper@Jaspers-Mac.local>
2018-01-27 16:43:22 -0800

    revises 7 again

--- curriculum/js1/7.js
+++ curriculum/js1/7.js
@@ -5,9 +5,23 @@
  * @returns {boolean}
  */
 
-const solution = (num) => {
-  return 0;
+const solution = (num, counter=2) => {
+  //checks for divisors up to half of num
+  if (counter > num/2) {
+    return console.log("num is prime")
+  }
+
+  //if check to add to sum if counter number is a divisor, if divisor then number is not prime
+  if (num % counter === 0) {
+    return console.log("num is not prime")
+  } 
+
+  //increment counter and run again
+  counter += 1;
+  return solution(num, counter);
 };
+
+solution(18);
 
 module.exports = {
   solution,
