b2585cf54239bd1ef3010e23bc65ac4efb86f3c4
Jasper <Jasper@Jaspers-Mac.local>
2018-01-27 16:54:46 -0800

    solves 8

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -5,9 +5,25 @@
  * @returns {number}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a, b, counter = 1, GCD = 1) => {
+  //stops once counter reaches the lesser of a and b
+  if (b > a && counter > a) {
+    return console.log("GCD is: "+GCD);
+  } else if (a > b && counter > b) {
+    return console.log("GCD is: "+GCD);
+  }
+
+  //if a & b share a divisor it is the new GCD
+  if (a % counter === 0 && b % counter === 0) {
+    GCD = counter;
+  }
+  
+  //increment and run again
+  counter++;
+  return solution(a, b, counter, GCD);
 };
+
+solution(150, 50);
 
 module.exports = {
   solution,
