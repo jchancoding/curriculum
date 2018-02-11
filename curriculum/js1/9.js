f51530d8865f98bf66b37077efce64728d264a98
Jasper <Jasper@Jaspers-Mac.local>
2018-02-10 16:33:06 -0800

    solves 9 and passes test

--- curriculum/js1/9.js
+++ curriculum/js1/9.js
@@ -8,19 +8,16 @@ const solution = (num, counter = num) => {
   //early elimination if num is a multiple of 7
   if (num % 7 === 0 && counter == num) {
     num += 7;
-    return console.log("Next num divisible by 7 is: "+num)
+    return num
   }
 
   //incrementally checks if counter is divisible by 7
   if (counter % 7 === 0) {
-    return console.log("Next num divisible by 7 is: "+counter);
+    return counter;
   }
 
-  counter++;
-  return solution(num, counter);
+  return solution(num, counter+1);
 };
-
-solution (12);
 
 module.exports = {
   solution,
