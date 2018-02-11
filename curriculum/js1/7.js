06ac881e8d64c9e082584ff9f9fad49be6ecf4fc
Jasper <Jasper@Jaspers-Mac.local>
2018-02-10 16:21:00 -0800

    solves 7 and passes tests

--- curriculum/js1/7.js
+++ curriculum/js1/7.js
@@ -6,22 +6,22 @@
  */
 
 const solution = (num, counter=2) => {
+  if (num === 1) {
+    return false;
+  }
   //checks for divisors up to half of num
   if (counter > num/2) {
-    return console.log("num is prime")
+    return true;
   }
 
   //if check to add to sum if counter number is a divisor, if divisor then number is not prime
   if (num % counter === 0) {
-    return console.log("num is not prime")
+    return false;
   } 
 
   //increment counter and run again
-  counter += 1;
-  return solution(num, counter);
+  return solution(num, counter+1);
 };
-
-solution(18);
 
 module.exports = {
   solution,
