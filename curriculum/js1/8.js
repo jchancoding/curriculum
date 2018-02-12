9122593749a58828bd8db89821639d5bdc6a705f
JasperC <jchancoding@gmail.com>
2018-02-11 22:01:26 -0800

    reduces base case lines

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -7,17 +7,13 @@
 
 const solution = (a, b, counter = 1, GCD = 1) => {
   //stops once counter reaches the lesser of a and b
-  if (b >= a && counter > a) {
+  if (counter > a || counter > b) {
     return GCD
-  } else if (a >= b && counter > b) {
-    return GCD
-  }
-
+  } 
   //if a & b share a divisor it is the new GCD
   if (a % counter === 0 && b % counter === 0) {
     GCD = counter;
   }
-  
   //increment and run again
   return solution(a, b, counter+1, GCD);
 };
