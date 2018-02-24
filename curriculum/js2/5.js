8649df32b111a4475e4470ff4808ea862045e77d
JasperC <jchancoding@gmail.com>
2018-02-24 15:08:44 -0800

    solves 5 and passes tests

--- curriculum/js2/5.js
+++ curriculum/js2/5.js
@@ -4,8 +4,18 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, idx = 0, max = '') => {
+  if (idx == a.length) {
+    return max;
+  }
+  if (idx == 0) {
+    max = a[idx];
+    return solution (a, idx + 1, max);
+  }
+  if (a[idx] > max) {
+    max = a[idx];
+  }
+  return solution(a, idx + 1, max);
 };
 
 module.exports = {
