f08149902f148c41c3a63be549c1f1e1fce33b4b
JasperC <jchancoding@gmail.com>
2018-02-24 14:55:43 -0800

    solves 3 and passes tests

--- curriculum/js2/3.js
+++ curriculum/js2/3.js
@@ -5,8 +5,32 @@
  * @returns {array}
  */
 
-const solution = (a) => {
-  return [];
+const solution = (a, idx = 0) => {
+  if (idx == a.length) {
+    return a;
+  }
+  if (primeCheck(a[idx]) == false) {
+    a[idx] = 1;
+  }
+  return solution(a, idx + 1);
+};
+
+const primeCheck = (num, counter=2) => {
+  if (num === 1) {
+    return false;
+  }
+  //checks for divisors up to half of num
+  if (counter > num/2) {
+    return true;
+  }
+
+  //if check to add to sum if counter number is a divisor, if divisor then number is not prime
+  if (num % counter === 0) {
+    return false;
+  } 
+
+  //increment counter and run again
+  return primeCheck(num, counter+1);
 };
 
 module.exports = {
