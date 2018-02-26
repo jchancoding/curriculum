8877f8adb4dc75ae8b6d0c9f059f8c6408a37a5d
JasperC <jchancoding@gmail.com>
2018-02-25 22:59:08 -0800

    solves 9

--- curriculum/js2/9.js
+++ curriculum/js2/9.js
@@ -4,11 +4,32 @@
  */
 
 const solution = () => {
-  Array.prototype.gsReduce = function () {
-    // To get the actual array, use this
-    return [];
-  };
-};
+  Array.prototype.gsReduce = function (callback, idx = 0, accumulator = 0) {
+    if (typeof idx != "number") {
+      return false;
+    }
+    if (idx == this.length) {
+      return accumulator;
+    }
+    if (typeof accumulator == "number") {
+      accumulator += callback(this[idx]);
+    }
+    console.log(Array.isArray(accumulator));
+    if (Array.isArray(accumulator) == true) {
+      accumulator.push(callback(this[idx]));
+    }
+    if (typeof accumulator == "object") {
+      accumulator[idx] = callback(this[idx]);
+    }
+
+    return this.gsReduce(callback, idx + 1, accumulator);
+  }     
+}
+
+solution();
+  
+var myarr = [1,2,3,4,5];
+console.log(myarr.gsReduce(function callback(element){return 2*element;}, 0, {}));
 
 module.exports = {
   solution,
