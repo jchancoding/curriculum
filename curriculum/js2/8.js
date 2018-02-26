5255345cad5c254b510a4488508f91489176a8df
JasperC <jchancoding@gmail.com>
2018-02-25 22:16:36 -0800

    solves 8 and passes test

--- curriculum/js2/8.js
+++ curriculum/js2/8.js
@@ -4,11 +4,19 @@
  */
 
 const solution = () => {
-  Array.prototype.gsMap = function () {
-    // To get the actual array, use this
-    return [];
-  };
-};
+  Array.prototype.gsMap = function (callback, result=[]) {
+    if (result.length == this.length) {
+      return result;
+    }
+    result.push(callback(this[result.length]));
+    return this.gsMap(callback, result);
+  }     
+}
+
+solution();
+
+var myarr = [1,2,3,4,5];
+console.log(myarr.gsMap((element) => 2 * element));
 
 module.exports = {
   solution,
