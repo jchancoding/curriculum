8c3e3b4f821d3ddc2c506865918dc3239700b988
JasperC <jchancoding@gmail.com>
2018-03-10 14:22:54 -0800

    fixes accumulator and comparison

--- curriculum/js2/9.js
+++ curriculum/js2/9.js
@@ -8,20 +8,10 @@ const solution = () => {
     if (typeof idx != "number") {
       return false;
     }
-    if (idx == this.length) {
+    if (idx === this.length) {
       return accumulator;
     }
-    if (typeof accumulator == "number") {
-      accumulator += callback(this[idx]);
-    }
-    console.log(Array.isArray(accumulator));
-    if (Array.isArray(accumulator) == true) {
-      accumulator.push(callback(this[idx]));
-    }
-    if (typeof accumulator == "object") {
-      accumulator[idx] = callback(this[idx]);
-    }
-
+    accumulator = callback(accumulator, this[idx]);
     return this.gsReduce(callback, idx + 1, accumulator);
   }     
 }
