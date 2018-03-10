e3529d7b3a2997adf99a867e57b4c38bdd0812a6
JasperC <jchancoding@gmail.com>
2018-03-10 13:58:08 -0800

    imports isPrime

--- curriculum/js2/3.js
+++ curriculum/js2/3.js
@@ -1,3 +1,4 @@
+const isPrime = require('../js1/7.js').solution;
 /**
  * Given an array, return an array of the same length
  *   where all elements that are not prime is changed to 1
@@ -9,28 +10,10 @@ const solution = (a, idx = 0) => {
   if (idx == a.length) {
     return a;
   }
-  if (primeCheck(a[idx]) == false) {
+  if (isPrime(a[idx]) == false) {
     a[idx] = 1;
   }
   return solution(a, idx + 1);
-};
-
-const primeCheck = (num, counter=2) => {
-  if (num === 1) {
-    return false;
-  }
-  //checks for divisors up to half of num
-  if (counter > num/2) {
-    return true;
-  }
-
-  //if check to add to sum if counter number is a divisor, if divisor then number is not prime
-  if (num % counter === 0) {
-    return false;
-  } 
-
-  //increment counter and run again
-  return primeCheck(num, counter+1);
 };
 
 module.exports = {
